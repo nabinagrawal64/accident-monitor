18427ee621552cfe6809ceeee0dc57784519f367
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-07-13 08:59:26 +0530

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -342,5 +342,5 @@ def stop_webcam():
 # ---------- Dev runner ----------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.0", port=8000)
 
